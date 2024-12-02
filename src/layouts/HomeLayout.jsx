2c09f19e2e2eac3644175978796a1a1a4dd64668
zoya <cse.zoya.2003@gmail.com>
2024-12-02 19:21:49 +0530

    added login signup button in sideBar

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 import {FiMenu} from "react-icons/fi";
 import {AiFillCloseCircle} from "react-icons/ai";
-import { Link } from "react-router-dom";
 import Footer from "../components/Footer.jsx";
 
 function HomeLayout({ children }){
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+    const role = useSelector((state) => state?.auth?.role);
 
     function changeWidth({children}) {
         const drawerSide = document.getElementsByClassName("drawer-side");
@@ -17,26 +26,40 @@ function HomeLayout({ children }){
         drawerSide[0].style.width = "0";
     }
 
+    function onLogout() {
+        //todo
+        e.preventdefault();
+        // to do
+        navigate("/");
+    }
+
     return(
         <div className="bg-pageBg min-h-[90vh]">
             <div className="drawer absolute left-0 z-50 w-full">
                 <input id="my-drawer" type="checkbox" className="drawer-toggle"/>
                 <div className="drawer-content">
                     <label htmlFor="my-drawer">
-                        <FiMenu onClick={changeWidth} size={"32px"} className="font-bold m-4"/>
+                        <FiMenu onClick={changeWidth} size={"32px"} className="text-white font-bold m-4"/>
                     </label>
                 </div>
                 <div className="drawer-side w-0">
                     <label htmlFor="my-drawer" className="drawer-overlay"></label> 
-                    <ul className="menu p-4 w-48 sm:w-80 bg-base-200 text-base-content relative">
+                    <ul className="menu p-4 h-[31vh] w-48 sm:w-80 bg-base-200 text-base-content relative">
                         <li className="m-fit absolute right-2 z-50">
-                            <button onClick={hideDrawer}>
+                            <button onClick={hideDrawer} >
                                 <AiFillCloseCircle size={"24px"} />
                             </button>
                         </li>
                         <li>
                             <Link to="/">Home</Link>
                         </li>
+                        { isLoggedIn && role === "ADMIN" && (
+                            <li>
+                                <Link to="/admin/dashboard">Admin Dashboard</Link>
+                            </li>
+                        )
+
+                        }
                         <li>
                             <Link to="/about">About Us</Link>
                         </li>
@@ -46,6 +69,31 @@ function HomeLayout({ children }){
                         <li>
                             <Link to="/courses">Courses</Link>
                         </li>
+
+                        { isLoggedIn ? (
+                            <li className="absolute bottom-4 w-[90%]">
+                                <div className="w-full flex items-center justify-center">
+                                    <button className="bg-blue-600 hover:bg-blue-800 text-white py-2 font-semibold rounded-md w-full">
+                                        Logout
+                                    </button>
+                                    <button className="bg-pink-600 hover:bg-pink-800 text-white py-2 font-semibold rounded-md w-full">
+                                        <Link to="/user/profile">Profile</Link>
+                                    </button>
+                                </div>
+                            </li>
+                        ) : (
+                            <li className="absolute bottom-4 w-[90%]">
+                                <div className="w-full flex items-center justify-center">
+                                    <button className="bg-blue-600 hover:bg-blue-800 text-white py-2 font-semibold rounded-md w-full">
+                                        <Link to="/login">Login</Link>
+                                    </button>
+                                    <button className="bg-pink-600 hover:bg-pink-800 text-white py-2 font-semibold rounded-md w-full">
+                                        <Link to="/signup">Signup</Link>
+                                    </button>
+                                </div>
+                            </li>
+                        )}
+
                     </ul>
                 </div>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
     
     return(
         <HomeLayout>
-            <div className="pt-10 flex items-center justify-center gap-10 mx-16 h-[90vh]">
+            <div className="text-white pt-10 flex items-center justify-center gap-10 mx-16 h-[90vh]">
                 <div className="w-1/2 space-y-6">
                     <h1 className="text-5xl font-semibold">Find out best <span className="text-yellow-500 font-bold">Online courses</span></h1>
                     <p className="text-xl">
@@ -14,7 +14,7 @@ function Home() {
                     </p>
                     <div className="space-x-6">
                         <Link to="/courses">
-                            <button className="bg-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-3000">
+                            <button className="bg-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
                                 Explore courses
                             </button>
                         </Link>
