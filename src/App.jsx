a990034b73d5380137bbb28a7cf68ddb161c48c5
zoya <cse.zoya.2003@gmail.com>
2024-12-03 11:34:48 +0530

    added notfond page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Aboutus from "./pages/Aboutus";
 import Footer from "./components/Footer";
+import Notfound from "./pages/Notfound";
 
 function App() {
     
@@ -10,6 +11,7 @@ function App() {
          <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/about' element={<Aboutus/>}/>
+            <Route path='*' element={<Notfound/>}/>
         </Routes>
     );
 }
